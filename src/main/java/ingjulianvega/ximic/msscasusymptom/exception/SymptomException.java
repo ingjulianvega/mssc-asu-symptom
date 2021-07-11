6a93fca6179811c5f136605e946bb56075752600
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:22:08 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasusymptom/exception/SymptomException.java
+++ src/main/java/ingjulianvega/ximic/msscasusymptom/exception/SymptomException.java
@@ -1,15 +1,19 @@
 package ingjulianvega.ximic.msscasusymptom.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SymptomException extends RuntimeException {
 
-    private final String code;
-
-    public SymptomException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 }
 
